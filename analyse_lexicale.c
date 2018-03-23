621380643f09b22bb96a11277e68a667ac25d9f2
Requiem <requiem958@users.noreply.github.com>
2018-03-23 11:11:52 +0100

    Ajout tout lexeme defini dans grammaire.txt
    A la date du 23/03/18

--- analyse_lexicale.c
+++ analyse_lexicale.c
@@ -20,7 +20,6 @@
 
 #include "lecture_caracteres.h"
 #include "analyse_lexicale.h"
-
 /* --------------------------------------------------------------------- */
 
 
@@ -33,6 +32,7 @@ int est_separateur(char c ) ;
 int est_chiffre(char c ) ;
 int est_symbole(char c ) ;
 void reconnaitre_lexeme();
+Nature_Lexeme symb_to_lex(char *chaine);
 Nature_Lexeme identificateur(char *chaine);
 /* --------------------------------------------------------------------- */
 
@@ -91,161 +91,172 @@ void reconnaitre_lexeme() {
   // on utilise ensuite un automate pour reconnaitre le prochain lexeme
 
   while (etat != E_FIN) {
-    
-    switch (etat) {
+	  switch (etat) {
+		  case E_INIT:
+			  if (fin_de_sequence_car() != 0) {
+				  lexeme_en_cours.nature = FIN_SEQUENCE;
+				  etat = E_FIN;
+			  }
+			  else {
+				  lexeme_en_cours.ligne = numero_ligne();
+				  lexeme_en_cours.colonne = numero_colonne();
+				  ajouter_caractere(lexeme_en_cours.chaine, caractere_courant());
 
-    case E_INIT:
-      if (fin_de_sequence_car() != 0) {
-	lexeme_en_cours.nature = FIN_SEQUENCE;
-	etat = E_FIN;
-      }
-      else {
-	lexeme_en_cours.ligne = numero_ligne();
-	lexeme_en_cours.colonne = numero_colonne();
-	ajouter_caractere(lexeme_en_cours.chaine, caractere_courant());
-
-	if (caractere_courant() == ';'){
-	  lexeme_en_cours.nature = FIN_INSTR;
-	  etat = E_FIN_INSTR;
-	}
-	else if (caractere_courant() == '.'){
-	  lexeme_en_cours.nature = NUM;
-	  etat = E_FLOAT;
-	}
-	else if (est_chiffre(caractere_courant())) {
-	  lexeme_en_cours.nature = NUM;
-	  lexeme_en_cours.valeur.val_i = caractere_courant() - '0';
-	  etat = E_ENTIER;
-	}
-	else if  (est_symbole(caractere_courant())) {
-	  switch (caractere_courant()){
-	  case '+':
-	    lexeme_en_cours.nature = PLUS;
-	    break;
-	  case '-':
-	    lexeme_en_cours.nature = MOINS;
-	    break;
-	  case '*':
-	    lexeme_en_cours.nature = MUL;
-	    break;
-	  case '/':
-	    lexeme_en_cours.nature = DIV;
-	    break;
-	  case ')':
-	    lexeme_en_cours.nature = PARF;
-	    break;
-	  case '(':
-	    lexeme_en_cours.nature = PARO;
-	    break;
-	  case '=':
-	    lexeme_en_cours.nature = EQUAL;
-	    break;
-	  default:
-	    lexeme_en_cours.nature = ERREUR;
-	    break;
+				  //Reconnaissance fin de l'instruction
+				  if (caractere_courant() == ';'){
+					  lexeme_en_cours.nature = FIN_INSTR;
+					  etat = E_FIN_INSTR;
+				  }
+				  //Reconnaissance Numérique
+				  else if (caractere_courant() == '.'){
+					  lexeme_en_cours.nature = NUM;
+					  etat = E_FLOAT;
+				  }
+				  else if (est_chiffre(caractere_courant())) {
+					  lexeme_en_cours.nature = NUM;
+					  lexeme_en_cours.valeur.val_i = caractere_courant() - '0';
+					  etat = E_ENTIER;
+				  }
+				  //Reconnaissance opérateur symbole
+				  else if  (est_symbole(caractere_courant())) {
+					  avancer_car();
+					  while (est_symbole(caractere_courant()) ){
+						  ajouter_caractere( lexeme_en_cours.chaine, caractere_courant() );
+						  avancer_car();
+					  }
+					  lexeme_en_cours.nature = symb_to_lex(lexeme_en_cours.chaine);
+					  etat = E_FIN;
+				  }
+				  //Reconnaissance chaine de caractères sans symboles
+				  else {
+					  lexeme_en_cours.chaine[strlen(lexeme_en_cours.chaine)] = '\0';
+					  avancer_car();
+					  while (!est_separateur(caractere_courant()) 	&&\
+							 !fin_de_sequence_car() 			&&\
+							 !est_symbole(caractere_courant()) 	&&\
+							 !(caractere_courant() == ';')
+							){
+						  ajouter_caractere (lexeme_en_cours.chaine,\
+											 tolower(caractere_courant())) ;
+						  avancer_car();
+					  }
+					  ajouter_caractere( lexeme_en_cours.chaine, '\0');
+					  etat = E_CAR;
+					  goto car;
+				  }
+			  }
+			  avancer_car() ;
+			  break ;
+		  case E_ENTIER:
+			  lexeme_en_cours.type = INT;
+			  if (!est_chiffre(caractere_courant()) && !(caractere_courant()=='.'))
+				  etat=E_FIN;
+			  else{
+				  ajouter_caractere(lexeme_en_cours.chaine,caractere_courant());
+				  if (est_chiffre(caractere_courant())) {
+					  lexeme_en_cours.valeur.val_i *= 10 ;
+					  lexeme_en_cours.valeur.val_i += caractere_courant() - '0';
+					  etat = E_ENTIER;
+				  }
+				  else if (caractere_courant()== '.'){
+					  lexeme_en_cours.valeur.val_f=lexeme_en_cours.valeur.val_i;
+					  etat=E_FLOAT;
+				  }
+				  avancer_car();
+			  }
+			  break;
+		  case E_FLOAT:
+			  lexeme_en_cours.type = FLOAT;
+			  if (est_chiffre(caractere_courant())){
+				  lexeme_en_cours.valeur.val_f += (caractere_courant() -'0')*exp;
+				  ajouter_caractere (lexeme_en_cours.chaine, caractere_courant()) ;
+				  exp=exp*0.1;
+				  avancer_car ();
+			  }
+			  else
+				  etat=E_FIN;
+			  break;
+car:
+		  case E_CAR:
+			  lexeme_en_cours.nature = identificateur(lexeme_en_cours.chaine);;
+			  etat = E_FIN;
+			  break;
+		  case E_FIN_INSTR: //On ne considère plus rien après ;; (ocaml sur turing)
+			  ajouter_caractere (lexeme_en_cours.chaine, caractere_courant()) ;
+			  if (strcmp(lexeme_en_cours.chaine, ";;") != 0 )
+				  lexeme_en_cours.nature = ERREUR;
+			  else
+				  lexeme_en_cours.nature = FIN_INSTR;
+			  avancer_car();
+			  etat = E_FIN;
+			  break;
+		  case E_FIN:
+			  break ;
 	  }
-	  etat = E_FIN;
-	}
-	else {
-	  lexeme_en_cours.chaine[strlen(lexeme_en_cours.chaine)] = '\0';
-	  avancer_car();
-	  while (!est_separateur(caractere_courant()) &&		\
-		 !fin_de_sequence_car() &&				\
-		 !est_symbole(caractere_courant()) &&			\
-		 !(caractere_courant() == ';')
-		 ){
-	    ajouter_caractere (lexeme_en_cours.chaine,			\
-			       tolower(caractere_courant())) ;
-	    avancer_car();
-	  }
-	  ajouter_caractere( lexeme_en_cours.chaine, 0);
-	  etat = E_CAR;
-	  goto car;
-	}
-      }
-      avancer_car() ;
-      break ;
-
-    case E_ENTIER:
-      lexeme_en_cours.type = INT;
-      if (!est_chiffre(caractere_courant()) && !(caractere_courant()=='.'))
-	etat=E_FIN;
-      else{
-	ajouter_caractere(lexeme_en_cours.chaine,caractere_courant());
-	if (est_chiffre(caractere_courant())) {
-	  lexeme_en_cours.valeur.val_i *= 10 ;
-	  lexeme_en_cours.valeur.val_i += caractere_courant() - '0';
-	  etat = E_ENTIER;
-	}
-	else if (caractere_courant()== '.'){
-	  lexeme_en_cours.valeur.val_f=lexeme_en_cours.valeur.val_i;
-	  etat=E_FLOAT;
-	}
-	avancer_car();
-      }
-      break;
-
-    case E_FLOAT:
-      lexeme_en_cours.type = FLOAT;
-      if (est_chiffre(caractere_courant())){
-	lexeme_en_cours.valeur.val_f += (caractere_courant() -'0')*exp;
-	ajouter_caractere (lexeme_en_cours.chaine, caractere_courant()) ;
-	exp=exp*0.1;
-	avancer_car ();
-      }
-      else
-	etat=E_FIN;
-      break;
-
-    car:
-    case E_CAR:
-      lexeme_en_cours.nature = identificateur(lexeme_en_cours.chaine);;
-      etat = E_FIN;
-      break;
-
-    case E_FIN_INSTR: //On ne considère plus rien après ;; (ocaml sur turing)
-      ajouter_caractere (lexeme_en_cours.chaine, caractere_courant()) ;
-      if (strcmp(lexeme_en_cours.chaine, ";;") != 0 )
-	lexeme_en_cours.nature = ERREUR;
-      else
-	lexeme_en_cours.nature = FIN_INSTR;
-      avancer_car();
-      etat = E_FIN;
-      break;
-      
-    case E_FIN:
-      break ;
   }
-}
 }
 
 /* --------------------------------------------------------------------- */
 
+#define IF_IDF(x) strcmp(chaine,x) == 0)
+
 Nature_Lexeme identificateur(char *chaine){
-  if (strcmp(chaine,"let") == 0)
+  IF_IDF("let")
     return LET;
-  else if (strcmp(chaine,"and") == 0)
+  else IF_IDF("and")
     return AND;
-  else if ( strcmp(chaine,":bool") == 0 )
+	else IF_IDF("or")
+		return LOGIC_OR;
+  else IF_IDF(":bool")
     return TYPE;
-  else if ( strcmp(chaine,":int") == 0 )
+  else IF_IDF(":int")
     return TYPE;
-  else if ( strcmp(chaine,":float") == 0 )
+  else IF_IDF(":float")
     return TYPE;
-  else if ( strcmp(chaine,":string") == 0 )
+  else IF_IDF(":string")
     return TYPE;
-  else if ( strcmp(chaine,"in") == 0 )
+  else IF_IDF("in")
     return IN;
-  else if ( strcmp(chaine,"function") == 0 )
+  else IF_IDF("function")
     return FUNCTION;
-  else if ( strcmp(chaine,"begin") == 0 )
+  else IF_IDF("begin")
     return PARO;
-  else if ( strcmp(chaine,"end") == 0 )
+  else IF_IDF("end")
     return PARF;
   else
     return VAR;
 }
 
+Nature_Lexeme symb_to_lex(char *chaine){
+	IF_IDF('+')
+		return PLUS;
+	else IF_IDF('-')
+		return MOINS;
+	else IF_IDF('*')
+		return MUL;
+	else IF_IDF('/')
+		return DIV;
+	else IF_IDF('&')
+		return LOGIC_AND;
+	else IF_IDF(')')
+		return PARF;
+	else IF_IDF('(')
+		return PARO;
+	else IF_IDF('=')
+		return EQUAL;
+	else IF_IDF('>')
+		return GREATER_THAN;
+	else IF_IDF('<')
+		return LOWER_THAN;
+	else IF_IDF('>=')
+		return GREATER_OR_EQUAL;
+	else IF_IDF('<=')
+		return LOWER_OR_EQUAL;
+	else IF_IDF('!=')
+		return INEQUALITY;
+	else
+		return ERREUR;
+}
 // cette fonction ajoute le caractere c a la fin de la chaine s 
 // (la chaine s est donc modifiee)
 
@@ -275,43 +286,75 @@ int est_chiffre(char c) {
 
 // vaut vrai ssi c designe un caractere correspondant a un symbole arithmetique
 int est_symbole(char c)  {
-  switch (c) {
-  case '+':
-  case '-':
-  case '*':
-  case '/':
-  case '(':
-  case ')':
-  case '=':
-    return 1;
-
-  default:
-    return 0;
+	switch (c) {
+		case '+':
+		case '-':
+		case '*':
+		case '/':
+		case '(':
+		case ')':
+		case '=':
+		case '<':
+	  	case '>':
+		case '!':
+		case '&':
+		case '|':
+			return 1;
+		default:
+    	return 0;
   } 
 }
 
 /* --------------------------------------------------------------------- */
 
 // renvoie la chaine de caracteres correspondant a la nature du lexeme
+
+#define LEX_NAT_CHN(x) case x: return #x
+#define 
+
 char *Nature_vers_Chaine (Nature_Lexeme nature) {
-  switch (nature) {
-  case NUM: return "NUM" ;
-  case TYPE: return "TYPE";
-  case PLUS: return "PLUS" ;
-  case MOINS: return "MOINS" ;
-  case MUL: return "MUL" ;
-  case DIV: return "DIV";
-  case IN:return "IN";
-  case FUNCTION:return "FUNCTION";
-  case EQUAL: return "EQUAL";
-  case LET: return "LET";
-  case AND: return "AND";
-  case FIN_INSTR: return "FIN_INSTR" ;
-  case PARO: return "PARO";
-  case PARF: return "PARF";
-  case VAR: return "VAR";
-  case FIN_SEQUENCE: return "FIN_SEQUENCE"; 
-  default: return "ERREUR" ;
+	switch (nature) {
+			LEX_NAT_CHN(PLUS);             // +
+			LEX_NAT_CHN(MOINS);            // -
+			LEX_NAT_CHN(MUL);		           // *
+  		LEX_NAT_CHN(DIV);              // /
+  
+  //Logic op
+  		LEX_NAT_CHN(LOGIC_AND);
+  		LEX_NAT_CHN(LOGIC_OR);
+  		LEX_NAT_CHN(NOT);
+  
+  //Comparison ops
+  		LEX_NAT_CHN(EQUALITY);
+  		LEX_NAT_CHN(INEQUALITY);
+  		LEX_NAT_CHN(GREATER_THAN);
+  		LEX_NAT_CHN(LESS_THAN);
+  		LEX_NAT_CHN(GREAT_OR_EQUAL);
+  		LEX_NAT_CHN(LESS_OR_EQUAL);
+  
+  //Delimiters
+  		LEX_NAT_CHN(PARO);    //(
+  		LEX_NAT_CHN(PARF);    //)
+  
+  //Various
+  		LEX_NAT_CHN(LET); //let
+  		LEX_NAT_CHN(AND); //and
+  		LEX_NAT_CHN(IN);// in
+  		LEX_NAT_CHN(EQUAL); //=
+  		LEX_NAT_CHN(IF);
+  		LEX_NAT_CHN(THEN);
+  		LEX_NAT(ELSE);
+  
+  //Data types
+  		LEX_NAT_CHN(VAR); //Variable
+  		LEX_NAT_CHN(FUNCTION); //fonction
+  		LEX_NAT_CHN(TYPE); //Data type
+  		LEX_NAT_CHN(NUM);// sequence de chiffres
+  
+  //control lex
+			LEX_NAT_CHN(FIN_INSTR); //;;
+			LEX_NAT_CHN(FIN_SEQUENCE);
+			default: return "ERREUR" ;
   }
 } 
 
