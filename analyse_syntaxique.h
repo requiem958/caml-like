3ce1030e4896a4e85e3b696ca9b6886ad73bf6df
Requiem <requiem958@users.noreply.github.com>
2018-03-30 10:43:03 +0200

    Update analyse_syntaxique.h

--- analyse_syntaxique.h
+++ analyse_syntaxique.h
@@ -5,18 +5,48 @@
 #include "analyse_lexicale.h"
 
 typedef enum {
+  //Jesavaispasouvousmettre
   ERR_PRG,
   ERR_FPRG,
   ERR_COMM,
+  //Expression
   ERR_SEXPR,
   ERR_SSEXPR,
   ERR_EXPR,
-  ERR_COND,
-  ERR_COMP,
-  ERR_VAL,
+  //Affectation
+  ERR_SAFF,
+  ERR_SSAFF,
   ERR_AFF,
-  ERR_AFFS,
-  ERR_SAFFS,
+  ERR_EQU,
+  ERR_OBJ,
+  //Operations
+  ERR_OPE,
+  ERR_STERME,
+  ERR_SSTERME,
+  ERR_TERME,
+  ERR_SFACTEUR,
+  ERR_SSFACTEUR,
+  ERR_FACTEUR,
+  ERR_VAL,
+  ERR_NOMVAR,
+  ERR_IDF,
+  /*Condition */
+  ERR_COND,
+  ERR_THEN,
+  ERR_SCOND,
+  
+  ERR_SBOOLEAU,
+  ERR_SSBOOLEAU,
+  ERR_BOOLEAU,
+  ERR_LOR,
+  
+  ERR_S_BOOLAND,
+  ERR_SSBOOLAND,
+  ERR_BOOLAND,
+  
+  ERR_COMP,
+  ERR_OPCOMP,
+  
   ERR_INSTR,
   ERR_INIT,
   ERR_SINIT,
@@ -24,12 +54,6 @@ typedef enum {
   ERR_TYPE,
   ERR_OP1,
   ERR_OP2,
-  ERR_STERME,
-  ERR_SSTERME,
-  ERR_TERME,
-  ERR_SFACTEUR,
-  ERR_SSFACTEUR,
-  ERR_FACTEUR,
   ERR_LET,
   ERR_PARF,
   NOERR
