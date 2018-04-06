a48c6c0c4e0ee306f641e7937347472196e3088a
Zarukin <37689537+Zarukin@users.noreply.github.com>
2018-04-06 08:55:50 +0200

    yo

--- tabvar.c
+++ tabvar.c
@@ -1,1 +1,23 @@
-xdrtdtr
+#include <stdio.h>
+#include "analyse_lexical.h"
+#include "tabvar.h"
+
+void ajouter_var (Variable *w,MemVar *m){
+  Valeur res;
+  int index;
+  if (index=chercher_var(w->nom,m,res)!=-1)
+    m->tab[index]=*w;
+  else {
+    if ( m->taille >999)
+      printf ("erreur de segmentation\n");
+    
+  }
+    
+  
+  
+  
+}
+
+int chercher_var (char *var,MemVar m,Valeur res);
+
+void copie_environement (MemVar m ,MemVar m2);
