4655228eb827939161afb1f531f524babc210ccf
Zarukin <37689537+Zarukin@users.noreply.github.com>
2018-04-06 08:36:39 +0200

    ajout prototype et type  MEM

--- tabvar.h
+++ tabvar.h
@@ -1,7 +1,21 @@
+#include "analyse_lexical.h"
+#DEFINE MAXMEM 100
 
-#include "analysesyntaxique.h"
 
+typedef struct {
+  Valeur val;
+  Type t;
+  char *nom;
+} Variable;
 
+typedef struct {
+  Variable tab[MAXMEM];
+  int taille;
+} MemVar;
+
+void ajouter_var (Variable w);
+
+void chercher_var (char *var);
 
 
 
